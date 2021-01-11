f84ece691dca97e4bb1665710bb39542f692ffcf
mutuakennedy <mutuakennedy81@gmail.com>
2021-01-11 21:57:54 +0300

    set degub to true for dev settings

--- homey/settings/dev.py
+++ homey/settings/dev.py
@@ -2,7 +2,7 @@ from homey.settings.base import *
 import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
