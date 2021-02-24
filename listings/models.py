6cf0bec5c1c434c4255a186415c3b7c397458901
mutuakennedy <mutuakennedy81@gmail.com>
2021-02-24 14:05:44 +0300

    Listings app model fields updates for error less form uploads. 1. added and set blank and null values to True for the fields. 2. set default to No for the deal_closed field. 3. Commented out the __str__ function. 4. added convert to RGB to compress image method. This will allow upload of png files.

--- listings/models.py
+++ listings/models.py
@@ -38,10 +38,10 @@ class HomeType(PropertyType):
 		verbose_name_plural = 'HomeTypes'
 
 class PropertyBase(models.Model):
-	property_name = models.CharField(max_length=20, default=None, db_index=True)
+	property_name = models.CharField(max_length=120, default=None, db_index=True)
 	price = models.PositiveIntegerField(default=0)
-	virtual_tour_url = models.URLField(default = None)
-	location_name = models.CharField(max_length= 20, default=None)
+	virtual_tour_url = models.URLField(default = None, null=True, blank=True)
+	location_name = models.CharField(max_length= 120, default=None)
 	location = models.PointField(srid=4326, default=None)
 	description = models.TextField()
 	floor_area = models.PositiveIntegerField(blank = True, default = 1.0)
@@ -66,14 +66,14 @@ class PropertyBase(models.Model):
 		('YES', 'yes'),
 		('NO', 'No'),
 	)
-	deal_closed = models.CharField( choices=DEAL_CLOSED_CHOICES, max_length = 3, default='YES')
+	deal_closed = models.CharField( choices=DEAL_CLOSED_CHOICES, max_length = 3, default='NO')
 	final_closing_offer = models.PositiveIntegerField(default=None, blank=True, null=True)
 
 	class Meta:
 		abstract = True
-
-	def __str__(self):
-		return self.property_name + '-' + '-' + self.phone + '-' + self.email
+	#
+	# def __str__(self):
+	# 	return self.property_name + '-' + self.phone + '-' + self.email
 
 	@property
 	def longitude(self):
@@ -208,30 +208,30 @@ class Home(PropertyBase):
 	bathrooms = models.PositiveIntegerField(default=1, blank = True)
 	bedrooms = models.PositiveIntegerField(default=1)
 	total_rooms = models.PositiveIntegerField(default = 1, blank = False)
-	floor_number = models.PositiveIntegerField(default = 0)
-	number_of_stories = models.PositiveIntegerField(default = 0)
-	parking_spaces = models.PositiveIntegerField(default = 1)
-	year_built = models.PositiveIntegerField(blank = False, default = 2000)
+	floor_number = models.PositiveIntegerField(default = 0,blank = True, null=True)
+	number_of_stories = models.PositiveIntegerField(default = 0, blank = True, null=True)
+	parking_spaces = models.PositiveIntegerField(default = 1,null=True, blank = True)
+	year_built = models.PositiveIntegerField(blank = False, default = 2000, null=True)
 	remodel_year = models.PositiveIntegerField(blank = True, null = True)
-	garage_sqm = models.PositiveIntegerField(blank = True, default = 1)
+	garage_sqm = models.PositiveIntegerField(blank = True, default = 1,null=True)
 	#Additional Details#
 	#ROOM DETAILS
-	appliances = MultiSelectField(choices = APPLIANCES_CHOICES, default = None, blank = True)
-	basement = models.CharField(max_length = 10, choices = BASEMENT_CHOICES, default = None, blank = True)
-	floor_covering = MultiSelectField(choices = FLOOR_COVERING_CHOICES, default = None, blank = True)
-	rooms = MultiSelectField(choices = ROOMS_CHOICES, default = None, blank = True)
-	indoor_features = MultiSelectField(choices = INDOOR_FEATURES_CHOICES, default = None, blank = True)
+	appliances = MultiSelectField(choices = APPLIANCES_CHOICES, default = None, blank = True, null=True)
+	basement = models.CharField(max_length = 10, choices = BASEMENT_CHOICES, default = None, blank = True,null=True)
+	floor_covering = MultiSelectField(choices = FLOOR_COVERING_CHOICES, default = None, blank = True,null=True)
+	rooms = MultiSelectField(choices = ROOMS_CHOICES, default = None, blank = True,null=True)
+	indoor_features = MultiSelectField(choices = INDOOR_FEATURES_CHOICES, default = None, blank = True,null=True)
 	#UTILITY DETAILS
-	cooling_type = MultiSelectField(choices = COOLING_TYPE_CHOICES, default = None, blank = True)
-	heating_type = MultiSelectField(choices = HEATING_TYPE_CHOICES, default = None, blank = True)
-	heating_fuel = MultiSelectField(choices = HEATING_FUEL_CHOICES, default = None, blank = True)
+	cooling_type = MultiSelectField(choices = COOLING_TYPE_CHOICES, default = None, blank = True, null=True)
+	heating_type = MultiSelectField(choices = HEATING_TYPE_CHOICES, default = None, blank = True, null=True)
+	heating_fuel = MultiSelectField(choices = HEATING_FUEL_CHOICES, default = None, blank = True, null=True)
 	#BUILDING DETAILS
-	building_amenities = MultiSelectField(choices = BUILDING_AMENITIES_CHOICES, default = None, blank = True)
-	exterior = MultiSelectField(choices = EXTERIOR_CHOICES, default = None, blank = True)
-	outdoor_amenities = MultiSelectField(choices = OUTDOOR_AMENITIES_CHOICES, default = None, blank = True)
-	parking = MultiSelectField(choices = PARKING_CHOICES, default = None, blank = True)
-	roof = MultiSelectField(choices = ROOF_CHOICES, default = None, blank = True)
-	view = MultiSelectField(choices = VIEW_CHOICES, default = None, blank = True)
+	building_amenities = MultiSelectField(choices = BUILDING_AMENITIES_CHOICES, default = None, blank = True, null=True)
+	exterior = MultiSelectField(choices = EXTERIOR_CHOICES, default = None, blank = True, null=True)
+	outdoor_amenities = MultiSelectField(choices = OUTDOOR_AMENITIES_CHOICES, default = None, blank = True, null=True)
+	parking = MultiSelectField(choices = PARKING_CHOICES, default = None, blank = True, null=True)
+	roof = MultiSelectField(choices = ROOF_CHOICES, default = None, blank = True, null=True)
+	view = MultiSelectField(choices = VIEW_CHOICES, default = None, blank = True, null=True)
 
 	class Meta:
 		verbose_name_plural = 'Homes'
@@ -312,7 +312,7 @@ class PropertyPhoto(models.Model):
 		super(PropertyPhoto, self).save(*args, **kwargs)
 
 	def compressImage(self,image):
-		imageTemporary	=	Image.open(image)
+		imageTemporary	=	Image.open(image).convert('RGB')
 		outputIoStream	=	BytesIO()
 		imageTemporaryResized	=	imageTemporary.resize( (1020,573) ) # Resize can be set to various varibale values in settings.py
 		imageTemporary.save(outputIoStream , format='JPEG', quality=70) # change quality according to requirement.
