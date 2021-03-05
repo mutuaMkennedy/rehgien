43a876b31285dc86d110762f4a04e5d5b2d433c7
mutuakennedy <mutuakennedy81@gmail.com>
2021-03-05 13:30:06 +0300

    Added new url paths for property and video api views

--- listings/apis/urls.py
+++ listings/apis/urls.py
@@ -15,6 +15,21 @@ urlpatterns = [
     path('listings/property/home/<int:pk>/edit/', views.HomeUpdateApi.as_view(), name='home_update_api'),
     path('listings/property/home/<int:pk>/save/', views.HomeSavesSerializer.as_view(), name='home_saves_api'),
     path('listings/property/home/<int:pk>/delete/', views.HomeDeleteApi.as_view(), name='home_delete_api'),
+
+    #Property Photo endpoints
+    path('listings/property/home/photos/', views.PropertyPhotoListApi.as_view(), name='PropertyPhotoListApi'),
+    path('listings/property/home/photo/create/', views.PropertyPhotoCreateApi.as_view(), name='PropertyPhotoCreateApi'),
+    path('listings/property/home/photo/<int:pk>/retrieve/', views.PropertyPhotoDetailApi.as_view(), name='PropertyPhotoDetailApi'),
+    path('listings/property/home/photo/<int:pk>/edit/', views.PropertyPhotoUpdateApi.as_view(), name='PropertyPhotoUpdateApi'),
+    path('listings/property/home/photo/<int:pk>/delete/', views.PropertyPhotoDeleteApi.as_view(), name='PropertyPhotoDeleteApi'),
+
+    #Property Video endpoints
+    path('listings/property/home/videos/', views.PropertyVideoListApi.as_view(), name='PropertyVideoListApi'),
+    path('listings/property/home/video/create/', views.PropertyVideoCreateApi.as_view(), name='PropertyVideoCreateApi'),
+    path('listings/property/home/video/<int:pk>/retrieve/', views.PropertyVideoDetailApi.as_view(), name='PropertyVideoDetailApi'),
+    path('listings/property/home/video/<int:pk>/edit/', views.PropertyVideoUpdateApi.as_view(), name='PropertyVideoUpdateApi'),
+    path('listings/property/home/video/<int:pk>/delete/', views.PropertyVideoDeleteApi.as_view(), name='PropertyVideoDeleteApi'),
+
     #User Interactions Endpoints
     path('listings/property/property_interactions/list/', views.PropertyInteractionListApi.as_view(), name='property_interactions_list_api'),
     path('listings/property/property_interactions/create/', views.PropertyInteractionCreateApi.as_view(), name='property_interactions_create_api'),
