89ec2180e17e7ec5db48d40fb862a6f2debc1220
mutuakennedy <mutuakennedy81@gmail.com>
2021-03-05 13:25:26 +0300

    Added new fields to serializers and created a new Home model serializer
    New home mode serializer is a duplicate of the existing serializer but 
    does not nest property photos and videos in its response.
    
    This serializer class will be used by api views which dont require the 
    media fields

--- listings/apis/serializers.py
+++ listings/apis/serializers.py
@@ -115,12 +115,12 @@ class HomeTypeSerializer(serializers.ModelSerializer):
 class PropertyPhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.PropertyPhoto
-        fields = ['photo']
+        fields = ['pk','home', 'photo']
 
 class PropertyVideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.PropertyVideo
-        fields = ['video']
+        fields = ['pk','home','video']
 
 class PropertyOpenHouseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -137,6 +137,87 @@ class PropertyInteractionSerializer(serializers.ModelSerializer):
 class HomeSerializer(WritableNestedModelSerializer):
     home_photos= PropertyPhotoSerializer(many=True)
     home_video=PropertyVideoSerializer(many=True)
+    home_interactions = PropertyInteractionSerializer(many=True)
+    home_openhouse = PropertyOpenHouseSerializer(many=True)
+    cooling_type = serializers.MultipleChoiceField(choices=COOLING_TYPE_CHOICES, required=False)
+    appliances = serializers.MultipleChoiceField(choices=APPLIANCES_CHOICES, required=False)
+    floor_covering = serializers.MultipleChoiceField(choices=FLOOR_COVERING_CHOICES, required=False)
+    rooms = serializers.MultipleChoiceField(choices=ROOMS_CHOICES, required=False)
+    indoor_features = serializers.MultipleChoiceField(choices=INDOOR_FEATURES_CHOICES, required=False)
+    heating_type = serializers.MultipleChoiceField(choices=HEATING_TYPE_CHOICES, required=False)
+    heating_fuel = serializers.MultipleChoiceField(choices=HEATING_FUEL_CHOICES, required=False)
+    building_amenities = serializers.MultipleChoiceField(choices=BUILDING_AMENITIES_CHOICES, required=False)
+    exterior = serializers.MultipleChoiceField(choices=EXTERIOR_CHOICES, required=False)
+    outdoor_amenities = serializers.MultipleChoiceField(choices=OUTDOOR_AMENITIES_CHOICES, required=False)
+    parking = serializers.MultipleChoiceField(choices=PARKING_CHOICES, required=False)
+    roof = serializers.MultipleChoiceField(choices=ROOF_CHOICES, required=False)
+    view = serializers.MultipleChoiceField(choices=VIEW_CHOICES, required=False)
+    #read only method field
+    _total_saves_count_ = serializers.SerializerMethodField()
+    _listing_active_until_ = serializers.SerializerMethodField()
+    _total_views_count_ = serializers.SerializerMethodField()
+    _recent_views_count_ = serializers.SerializerMethodField()
+    _views_trend_ = serializers.SerializerMethodField()
+    class Meta:
+        model = models.Home
+        fields = [
+        'HOUSE_TYPE_CHOICES','BASEMENT_CHOICES','FLOOR_COVERING_CHOICES','ROOMS_CHOICES',
+        'INDOOR_FEATURES_CHOICES','COOLING_TYPE_CHOICES','HEATING_TYPE_CHOICES',
+        'HEATING_FUEL_CHOICES','BUILDING_AMENITIES_CHOICES','EXTERIOR_CHOICES',
+        'OUTDOOR_AMENITIES_CHOICES','PARKING_CHOICES','ROOF_CHOICES','VIEW_CHOICES',
+        'APPLIANCES_CHOICES','LISTING_TYPE_CHOICES','PROPERTY_CATEGORY_CHOICES',
+
+        'id','listing_type', 'property_category', 'property_name','price', 'home_type',
+        'virtual_tour_url', 'location_name', 'location', 'bathrooms',
+        'bedrooms', 'total_rooms','floor_number', 'description', 'floor_area',
+        'number_of_units', 'number_of_stories', 'home_photos','home_video','home_interactions',
+        'home_openhouse','saves', 'parking_spaces','year_built', 'remodel_year', 'garage_sqm','is_active',
+        'deal_closed', 'final_closing_offer',
+
+        'appliances', 'basement', 'floor_covering','rooms', 'indoor_features',
+        'cooling_type', 'heating_type', 'heating_fuel', 'building_amenities',
+        'exterior', 'outdoor_amenities', 'parking', 'roof',
+
+        'view', 'related_website', 'publishdate', 'phone', 'email','owner',
+        #read only method fields
+        '_total_saves_count_','_listing_active_until_','_total_views_count_', '_recent_views_count_',
+        '_views_trend_',
+        ]
+
+    def get__total_saves_count_(self, obj):
+        return obj.saves.count()
+
+    def get__listing_active_until_(self, obj):
+        return  obj.publishdate + datetime.timedelta(days = 30 )
+
+    def get__total_views_count_(self, obj):
+        count = obj.home_interactions.all().aggregate(Sum('views_count')).get('views_count__sum', 0)
+        if count == None:
+            return 0
+        else:
+            return count
+
+    def get__recent_views_count_(self, obj):
+        count = obj.home_interactions.filter(created_at__gte =  timezone.now() - datetime.timedelta(hours = 24) )\
+				.aggregate(Sum('views_count')).get('views_count__sum', 0)
+        if count == None:
+            return 0
+        else:
+            return count
+
+    def get__views_trend_(self, obj):
+        tt_views_array = []
+        for n_days in range(0,25): # in the last 24 hours
+            total_views = obj.home_interactions.filter(created_at__gte = timezone.now() - datetime.timedelta(hours = n_days) )\
+                         .aggregate(Sum('views_count')).get('views_count__sum', 0)
+            if total_views == None:
+                total_views = 0
+            tt_views_array.append(total_views)
+        views_index = list(range(1,len(tt_views_array)+1 ) )
+        trend_line = trend(views_index,tt_views_array)
+        return trend_line
+
+class HomeSerializer2(WritableNestedModelSerializer):
     home_interactions = PropertyInteractionSerializer(many=True)
     home_openhouse = PropertyOpenHouseSerializer(many=True)
     cooling_type = serializers.MultipleChoiceField(choices=COOLING_TYPE_CHOICES, required=False)
