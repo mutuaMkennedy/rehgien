2c4c6e543ff5e097a63e57119152f44894806c11
mutuakennedy <mutuakennedy81@gmail.com>
2021-03-16 22:54:09 +0300

    removed comment

--- homey/settings/base.py
+++ homey/settings/base.py
@@ -363,7 +363,7 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC' #'Africa/Nairobi'
+CELERY_TIMEZONE = 'UTC'
 CELERY_ENABLE_UTC = True
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
 
