fca3a475ff6dbde7b4878396ebf62b34f4f0f04d
mutuakennedy <mutuakennedy81@gmail.com>
2021-05-11 19:55:08 +0300

    Added url path to contact us page

--- contact/urls.py
+++ contact/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('contact_listing_agent/', views.contact_listing_agent, name='contact_listing_agent'),
     path('contact_professional/', views.contact_pro, name='contact_pro'),
     path('about_rehgien/', views.about_us, name='about_us'),
+    path('contact_us/', views.contact_us, name='contact_us'),
     path('share_home/', views.share_listing, name='share_listing'),
     path('report/problem/pg', views.page_report, name = 'page_report'),
     path('report/problem/p_p', views.p_p_report, name = 'p_p_report'),
