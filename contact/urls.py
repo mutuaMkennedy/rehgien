f598a32eae8cfa0c8134ff564d7fd3cfe978d8f0
mutuakennedy <mutuakennedy81@gmail.com>
2021-05-11 20:08:51 +0300

    removed redundant url path

--- contact/urls.py
+++ contact/urls.py
@@ -7,7 +7,6 @@ app_name = 'contact'
 urlpatterns = [
     path('contact_listing_agent/', views.contact_listing_agent, name='contact_listing_agent'),
     path('contact_professional/', views.contact_pro, name='contact_pro'),
-    path('about_rehgien/', views.about_us, name='about_us'),
     path('contact_us/', views.contact_us, name='contact_us'),
     path('share_home/', views.share_listing, name='share_listing'),
     path('report/problem/pg', views.page_report, name = 'page_report'),
