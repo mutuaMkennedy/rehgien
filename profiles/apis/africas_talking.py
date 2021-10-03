4fce72cb5b11b68d447712d600f0c35a63eeca03
mutuakennedy <mutuakennedy81@gmail.com>
2021-10-03 03:36:07 +0300

    fixed africa's talking sms send error. SMS's were failing to be sent because of an incorrect phone number and sender id value. Updating this sections fixed the error.

--- profiles/apis/africas_talking.py
+++ profiles/apis/africas_talking.py
@@ -4,11 +4,11 @@ sms = africastalking.SMS
 
 def send_otp_sms(otp_code):
     # Set the numbers in international format
-    recipients = ["+254717966627","0746424465"]
+    recipients = ["+254717966627","+254746424465"]
     # Set your message
     message = "Your phone verification code is: {otp}".format(otp=otp_code);
     # Set your shortCode or senderId
-    sender = "Rehgien"
+    sender = "3839"
     try:
         response = sms.send(message, recipients, sender)
         # print(response)
