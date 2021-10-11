133dcc598f678f528eceb728bcfc22224a4f3854
mutuakennedy <mutuakennedy81@gmail.com>
2021-10-12 00:11:46 +0300

    added reset password otp Africa's talking sms send function

--- profiles/apis/africas_talking.py
+++ profiles/apis/africas_talking.py
@@ -15,3 +15,17 @@ def send_otp_sms(otp_code):
         return True
     except Exception as e:
         return({'status': False, 'detail': f'OTP code not sent: {e}'})
+
+def send_password_reset_otp_sms(otp_code):
+    # Set the numbers in international format
+    recipients = ["+254717966627","+254746424465"]
+    # Set your message
+    message = "Your password reset code is: {otp}".format(otp=otp_code);
+    # Set your shortCode or senderId
+    sender = "3839"
+    try:
+        response = sms.send(message, recipients, sender)
+        # print(response)
+        return True
+    except Exception as e:
+        return({'status': False, 'detail': f'OTP code not sent: {e}'})
