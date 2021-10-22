c5fd18d5bfd705ebb224522b1a236147ce0d3ed1
mutuakennedy <mutuakennedy81@gmail.com>
2021-10-22 12:16:01 +0300

    Added comments and fixed imports

--- chat/views.py
+++ chat/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render,redirect, HttpResponseRedirect
 from django.contrib.auth import get_user_model
 from django.contrib import messages
-from profiles.models import AgentProfile
+from profiles.models import BusinessProfile
 from django.conf import settings
 from django.http import JsonResponse,HttpResponse
 from django.core.serializers import serialize
 import json
 import stream_chat
+
 # referencing the custom user model
 User = get_user_model()
 
+# TODO: Remove stream chat libraries and code
 STREAM_KEY = settings.STREAM_API_KEY
 STREAM_API_SECRET= settings.STREAM_API_SECRET
 
