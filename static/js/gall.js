042f4b66c3e70b833ebd66ed61443e60a18e158d
mutuakennedy <mutuakennedy81@gmail.com>
2021-11-22 00:52:36 +0300

    added canvas animation function

--- static/js/gall.js
+++ static/js/gall.js
@@ -35,6 +35,7 @@ $(document).ready(function(){
         // specify the path to the libphonenumber script to enable validation/formatting
         utilsScript: ""
   });
+
   //updates review message preview in rehgien_pro/templates/pro_reviewers
   $('.review_request_message').on('keyup', function() {
       $('#rvw_Rq_msg').html($(this).val());
@@ -50,5 +51,41 @@ $(document).ready(function(){
     $('#_helperTextTrigger').css('color','#000');
     $('#_helperText').css({'display':'none', 'transform':'translateY(0px)'});
   });
+
+  const canvas = document.getElementsByClassName('HomeHerocanvas');
+  if (canvas.length > 0) {
+    const context = canvas[0].getContext('2d');
+    let time = 0;
+
+    const color = function (x, y, r, g, b) {
+        context.fillStyle = `rgb(${r}, ${g}, ${b})`
+        context.fillRect(x, y, 10, 10);
+    }
+    const R = function (x, y, time) {
+        return (Math.floor(179 + 64 * Math.cos((x * x - y * y) / 300 + time)));
+    }
+
+    const G = function (x, y, time) {
+        return (Math.floor(20 + 64 * Math.sin((x * x * Math.cos(time / 4) + y * y * Math.sin(time / 3)) / 300)));
+    }
+
+    const B = function (x, y, time) {
+        return (Math.floor(185 + 64 * Math.sin(5 * Math.sin(time / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100)));
+    }
+
+    const startAnimation = function () {
+        for (x = 0; x <= 30; x++) {
+            for (y = 0; y <= 30; y++) {
+                color(x, y, R(x, y, time), G(x, y, time), B(x, y, time));
+            }
+        }
+        time = time + 0.03;
+        window.requestAnimationFrame(startAnimation);
+    }
+
+    startAnimation();
+  }
+
+
 
 });
