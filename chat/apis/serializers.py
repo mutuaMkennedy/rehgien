5eb92a5782ce7af3f0a959396a5252c1d069d4fc
mutuakennedy <mutuakennedy81@gmail.com>
2021-11-22 00:28:12 +0300

    removed get stream token field from serilizer. Get stream api is no longer used in the project

--- chat/apis/serializers.py
+++ chat/apis/serializers.py
@@ -1,30 +1,24 @@
 from rest_framework import serializers
-from stream_chat import StreamChat
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_auth.models import TokenModel
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from chat.twilio import conversations
 # referencing the custom user model
 User = get_user_model()
 
 
 class StreamTokenSerializer(serializers.ModelSerializer):
     auth_token = serializers.CharField(source='key')
-    stream_token = serializers.SerializerMethodField()
     user_id = serializers.SerializerMethodField()
     user_name = serializers.SerializerMethodField()
     user_avatar = serializers.SerializerMethodField()
     tokens_for_user = serializers.SerializerMethodField()
+    twilio_acess_token = serializers.SerializerMethodField()
 
     class Meta:
         model = TokenModel
-        fields = ('auth_token','stream_token','tokens_for_user','user_id', 'user_name','user_avatar')
-
-    def get_stream_token(self, obj):
-        client = StreamChat(api_key=settings.STREAM_API_KEY, api_secret=settings.STREAM_API_SECRET)
-        token = client.create_token(str(obj.user.pk))
-        return token
+        fields = ('auth_token','twilio_acess_token', 'tokens_for_user','user_id', 'user_name','user_avatar')
 
     def get_user_id(self, obj):
         return str(obj.user.pk)
@@ -38,6 +32,11 @@ class StreamTokenSerializer(serializers.ModelSerializer):
             return obj.user.profile_image.url
         else:
             return default_avatar
+
+    def get_twilio_acess_token(self, obj):
+        identity = obj.user.pk
+        token = conversations.get_token(identity)
+        return token
 
     def get_tokens_for_user(self, obj):
         """
