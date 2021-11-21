22c68f6df75d5fa68662cf347e3c27b9d9b3919b
mutuakennedy <mutuakennedy81@gmail.com>
2021-11-22 00:49:38 +0300

    Replaced azure chat urls with twilio chat urls.

--- chat/urls.py
+++ chat/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'chat'
 
 urlpatterns = [
-    url(r'user_token/$', views.user_token, name="user_token"),
-    path('pro_rooms/', views.chat_room, name='chat_room'),
+    path('twilio/get_token/', views.get_twilio_acess_token, name='get_twilio_acess_token'),
+    path('twilio/delete/conversation/', views.deleteConversation, name = 'deleteConversation'),
 ]
