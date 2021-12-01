6294a7663d41cbb0fd655ce1d366c1bec1f9eff4
mutuakennedy <mutuakennedy81@gmail.com>
2021-12-01 20:09:37 +0300

    Updated twilio send SMS function

--- contact/services/twilio_service.py
+++ contact/services/twilio_service.py
@@ -1,16 +1,21 @@
+from django.conf import settings
 from twilio.rest import Client
 
-class TwilioService:
-    client = None
+account_sid = settings.TWILIO_ACCOUNT_SID
+auth_token = settings.TWILIO_AUTH_TOKEN
+messaging_sid = settings.TWILIO_MESSAGING_SERVICE_SID
+twilio_phone_number = settings.TWILIO_PHONE_NUMBER
 
-    def __init__(self):
-        account_sid = 'AC1db0e8cfbae1e3b9b5834772c0ef8d6c'
-        auth_token = '7f70419841a1632045d657089acd65c1'
-        self.client = Client(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
-    def send_message(self, message,e_recepient_phone_number):
-        # agent_phone_number = '+254717966627'
-        twilio_phone_number = '+12054633293'
-        self.client.messages.create(to=e_recepient_phone_number,
-                                    from_=twilio_phone_number,
-                                    body=message)
+def send_SMS(message, recepient_phone_number):
+    try:
+        message = client.messages \
+            .create(
+                 body=message,
+                 messaging_service_sid=messaging_sid,
+                 to=recepient_phone_number
+             )
+        return True
+    except Exception as e:
+        return False
