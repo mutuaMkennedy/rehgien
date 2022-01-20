c96abb486870a79d4d2ff43213f363f6f162ac45
mutuakennedy <mutuakennedy81@gmail.com>
2022-01-20 17:11:47 +0300

    Added custom permission class

--- markets/apis/permissions.py
+++ markets/apis/permissions.py
@@ -6,6 +6,11 @@ class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self,request,view,obj):
         return obj.job_poster == request.user
 
+class IsProjectOwnerOrReadOnly(BasePermission):
+    message = 'Access denied: You are not the creator of this resource.'
+    def has_object_permission(self,request,view,obj):
+        return obj.owner == request.user
+
 # class IsPro(BasePermission):
 #     message = 'Permission denied. Your account is allowed to perfom this action.'
 #     def has_object_permission(self,request,view,obj):
