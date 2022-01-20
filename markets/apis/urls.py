342788b69963d66d887705c83faf0e70f5539b61
mutuakennedy <mutuakennedy81@gmail.com>
2022-01-20 17:14:53 +0300

    Added project model api view url endpoints

--- markets/apis/urls.py
+++ markets/apis/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('markets/job_post/<int:pk>/update/', views.JobPostUpdateApi.as_view(), name='JobPostUpdateApi'),
     path('markets/job_post/<int:pk>/add_viewer/', views.JobPostViewerUpdateApi.as_view(), name='JobPostViewerUpdateApi'),
     path('markets/job_post_proposal/create/', views.JobPostProposalCreateApi.as_view(), name='JobPostProposalCreateApi'),
+    # Project
+    path('markets/project/list/', views.ProjectListApi.as_view(), name='ProjectListApi'),
+    path('markets/project/create/', views.ProjectCreateApi.as_view(), name='ProjectCreateApi'),
+    path('markets/project/<int:pk>/retrieve/', views.ProjectDetailApi.as_view(), name='ProjectDetailApi'),
+    path('markets/project/<int:pk>/update/', views.ProjectUpdateApi.as_view(), name='ProjectUpdateApi'),
+
 ]
