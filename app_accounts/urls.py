cb859017e7788a9ec2f39a48444bad1884f24d14
mutuakennedy <mutuakennedy81@gmail.com>
2022-02-01 01:57:20 +0300

    Added url path for sign up views

--- app_accounts/urls.py
+++ app_accounts/urls.py
@@ -5,4 +5,7 @@ app_name = 'app_accounts'
 
 urlpatterns = [
     path('auth/login/', views.user_login, name='user_login'),
+    path('auth/signup/', views.user_signup, name='user_signup'),
+    path('auth/signup/setup/', views.UserAccountSetupWizardView.as_view(views.FORMS), name='UserAccountSetupWizard'),
+    path('validate_phone/',views.validate_phone_send_otp, name='validate_phone')
 ]
