ac6cceac90e63e09aa907332211a21b06120a303
mutuakennedy <mutuakennedy81@gmail.com>
2022-02-01 02:48:21 +0300

    feat: api views for notifications

---
+++ app_notifications/apis/views.py
@@ -0,0 +1,46 @@
+from . import serializers
+from rest_framework.generics import (
+                    ListAPIView,
+                    CreateAPIView,
+                    RetrieveAPIView,
+                    RetrieveUpdateAPIView,
+                    DestroyAPIView
+                    )
+from rest_framework.permissions import  (
+                    AllowAny,
+                    IsAuthenticated,
+                    IsAdminUser,
+                    IsAuthenticatedOrReadOnly
+                    )
+from . import permissions
+from app_notifications import models
+from notifications.models import Notification
+
+class NotificationsListApi(ListAPIView):
+    serializer_class = serializers.NotificationSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return Notification.objects.filter(recipient=user)
+
+class NotificationsUpdateApi(RetrieveUpdateAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = serializers.NotificationSerializer
+    permission_classes = [permissions.IsRecipient]
+
+class DeviceInformationListApi(ListAPIView):
+    serializer_class = serializers.DeviceInformationSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return models.DeviceInformation.objects.filter(user=user)
+
+class DeviceInformationCreateApi(CreateAPIView):
+    queryset = models.DeviceInformation.objects.all()
+    serializer_class = serializers.DeviceInformationSerializer
+    permission_classes = [IsAuthenticated]
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
+
+class DeviceInformationUpdateApi(RetrieveUpdateAPIView):
+    queryset = models.DeviceInformation.objects.all()
+    serializer_class = serializers.DeviceInformationSerializer
+    permission_classes = [permissions.IsUser]
