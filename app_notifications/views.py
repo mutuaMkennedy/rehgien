94964ae784e47f446e5a68df9d6132e1f86640f5
mutuakennedy <mutuakennedy81@gmail.com>
2022-02-20 03:41:00 +0300

    feat: add mark notifications as read view

--- app_notifications/views.py
+++ app_notifications/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
-# Create your views here.
+User = get_user_model()
+
+def mark_notifications_read(request):
+    user = User.objects.get(pk=request.user.pk)
+    user.notifications.mark_all_as_read()
+    notifications = user.notifications.all
+    context = {
+        "notifications":notifications
+    }
+    html = render_to_string('app_notifications/notifications_section.html', context, request=request)
+    return JsonResponse({'page_section':html})
