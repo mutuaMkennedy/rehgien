5fdbe869dfcd71560382f79f5d96d6786a0dac83
mutuakennedy <mutuakennedy81@gmail.com>
2022-03-06 14:39:50 +0300

    feat: add send quote API view

--- rehgien_pro/apis/views.py
+++ rehgien_pro/apis/views.py
@@ -4,6 +4,10 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from . import permissions 
+from rest_framework.permissions import  (
+                    IsAuthenticated,
+                    )
 from django.contrib.auth import get_user_model
 
 # referencing the custom user model
@@ -20,3 +24,15 @@ def LeadsListApi(request):
     else:
         message = {'Unauthorized': 'Your account is not authorized to access this API'}
         return Response(message, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class AddQuoteApi(generics.RetrieveUpdateAPIView):
+    queryset = markets_models.Project.objects.all()
+    serializer_class = serializers.ProjectSerializer
+    permission_classes = [IsAuthenticated, permissions.IsAPro]
+    # def perform_update(self,serializer):
+    #     print('1')
+    #     instance = serializer.save()
+    #     instance.project_quote.quote_sender = self.request.user
+    #     print('2')
+    #     # serializer.save(project_quote__quote_sender=self.request.user)
