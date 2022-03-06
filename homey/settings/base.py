abb6e2d8215303b6b26ecfcb8ef29f0e83454625
mutuakennedy <mutuakennedy81@gmail.com>
2022-03-06 17:24:47 +0300

    chore: update ngrok test url in allowed hosts

--- homey/settings/base.py
+++ homey/settings/base.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'ib)4kavk+9ds#_!v8y5*qoy2@)g1gfa@y2u(4tpvej$feb!!oj'
 
 ALLOWED_HOSTS = ['localhost','165.227.185.180','127.0.0.1','rehgiendev2.remote.moe',
-                '207.154.205.115','rehgien.com', 'www.rehgien.com', '197.156.137.177','607f-41-80-112-21.ngrok.io']
+                '207.154.205.115','rehgien.com', 'www.rehgien.com', '197.156.137.177','679d-41-80-112-236.ngrok.io']
 
 ADMINS = [('Mutua', 'do-not-reply@rehgien.com')]
 # Application definition
