eba1463d6df46d4a49365502989692507f20f450
mutuakennedy <mutuakennedy81@gmail.com>
2022-03-09 03:38:44 +0300

    feat: add mpesa payment api views

---
+++ payments/apis/views.py
@@ -0,0 +1,88 @@
+from ..mpesa import mpesa
+from rest_framework.decorators import api_view
+from rest_framework import permissions
+from django.contrib.auth import get_user_model
+import re
+from rest_framework import status
+from rest_framework.response import Response
+import json
+# referencing the custom user model
+User = get_user_model()
+
+phone_regex = r'^\+?1?\d{9,14}$'
+
+@api_view(['POST'])
+def pay_with_mpesa(request):
+    phone_number = request.POST.get('phone_number', '')
+    amount = int(request.POST.get('amount', 0))
+    if phone_number and amount:
+        if amount > 0:
+            if re.fullmatch(phone_regex,phone_number):
+                response = mpesa.pay_with_mpesa(phone_number,amount)
+                if status.is_success(response.status_code):
+                    # TO DO: Record this transaction in db
+                    # TO DO: Update project details to include payment details
+                    message = {
+                        'status': True,
+                        'message':["Payment sent sucessfully"],
+                        'details':[response.json()]
+                        }
+                    return Response(message, status=status.HTTP_200_OK)
+                else:
+                    message = {
+                        'status': False,
+                        'message':["Something went wrong try again later"],
+                        'details':[response.json()]
+                        }
+                    return Response(message, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            else:
+                message = {'phone_number': ['The phone number you provided is not valid']}
+                return Response(message, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            message = {'amount': ['Enter an amount that is larger than 0']}
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        message = {
+            'phone_number': ['This field is required'],
+            'amount': ['This field is required']
+            }
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def settle_mpesa_payment(request):
+    phone_number = request.POST.get('phone_number', '')
+    amount = int(request.POST.get('amount', 0))
+    if phone_number and amount:
+        if amount > 0:
+            if re.fullmatch(phone_regex,phone_number):
+                response = mpesa.settle_payment(phone_number, amount)
+                if status.is_success(response.status_code):
+                    # TO DO: Update this transaction in db
+                    # TO DO: Update project details to include payment details
+                    message = {
+                        'status': True,
+                        'message':["Payment sent sucessfully"],
+                        'details':[response.json()]
+                        }
+                    return Response(message, status=status.HTTP_200_OK)
+                else:
+                    message = {
+                        'status': False,
+                        'message':["Something went wrong try again later"],
+                        'details':[response.json()]
+                        }
+                    return Response(message, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            else:
+                message = {'phone_number': ['The phone number you provided is not valid']}
+                return Response(message, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            message = {'amount': ['Enter an amount that is larger than 0']}
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        message = {
+            'phone_number': ['This field is required'],
+            'amount': ['This field is required']
+            }
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
