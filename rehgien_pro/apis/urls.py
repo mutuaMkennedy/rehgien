de89d3e1a3e85a37f45968e53d59b40f4c3e0e7f
mutuakennedy <mutuakennedy81@gmail.com>
2022-03-22 20:17:36 +0300

    feat: add pro portfolio crud api endpoints

--- rehgien_pro/apis/urls.py
+++ rehgien_pro/apis/urls.py
@@ -7,4 +7,19 @@ urlpatterns = [
     path('pro/leads/list/', views.LeadsListApi, name='LeadsListApi'),
     path('pro/leads/<int:pk>/send_quote/', views.AddQuoteApi.as_view(), name='AddQuoteApi'),
 
+    # Targeting preferences
+    path('pro/targeting_preferences/update/', views.update_targeting_preferences, name='update_targeting_preferences'),
+
+    path('pro/reviews/email_request/', views.send_email_review_requests, name='send_email_review_requests'),
+
+    # Portflio items / Projects
+    path('pro/portfolio_item/list/', views.PortfolioItemListApi.as_view(), name='PortfolioItemListApi'),
+    path('pro/portfolio_item/create/', views.PortfolioItemCreateApi.as_view(), name='PortfolioItemCreateApi'),
+    path('pro/portfolio_item/<int:pk>/update/', views.PortfolioItemUpdateApi.as_view(), name='PortfolioItemUpdateApi'),
+    path('pro/portfolio_item/<int:pk>/delete/', views.PortfolioItemDeleteApi.as_view(), name='PortfolioItemDeleteApi'),
+
+    # Portflio items / Projects
+    path('pro/portfolio_item/photos/create/', views.PortfolioItemPhotoCreateApi.as_view(), name='PortfolioItemPhotoCreateApi'),
+    path('pro/portfolio_item/photos/<int:pk>/update/', views.PortfolioItemPhotoUpdateApi.as_view(), name='PortfolioItemPhotoUpdateApi'),
+    path('pro/portfolio_item/photos/<int:pk>/delete/', views.PortfolioItemPhotoDeleteApi.as_view(), name='PortfolioItemPhotoDeleteApi'),
 ]
