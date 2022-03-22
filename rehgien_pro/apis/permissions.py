ce76b004a7f1ef20d073ffaa2e1e2c9a83bf956e
mutuakennedy <mutuakennedy81@gmail.com>
2022-03-22 20:17:00 +0300

    feat: add custom base permissions

--- rehgien_pro/apis/permissions.py
+++ rehgien_pro/apis/permissions.py
@@ -8,3 +8,21 @@ class IsAPro(BasePermission):
 
     def has_object_permission(self,request,view,obj):
         return request.user == obj.pro_contacted
+
+class IsAPro2(BasePermission):
+    message = 'PermissionDenied.'
+    
+    def has_permission(self, request, view):
+        return request.user.user_type == 'PRO'
+
+    def has_object_permission(self,request,view,obj):
+        return request.user == obj.created_by
+
+class IsOwner(BasePermission):
+    message = 'PermissionDenied.'
+    
+    def has_permission(self, request, view):
+        return request.user.user_type == 'PRO'
+
+    def has_object_permission(self,request,view,obj):
+        return request.user == obj.portfolio_item.created_by
