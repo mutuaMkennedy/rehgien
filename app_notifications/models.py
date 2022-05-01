6718ca6ac7a40f5726ee32eb617064c91ad599b1
reiikennedy <mutuakennedy81@gmail.com>
2022-05-01 23:28:52 +0300

    feat: add supported app version model

--- app_notifications/models.py
+++ app_notifications/models.py
@@ -1,7 +1,8 @@
+from tabnanny import verbose
 from django.db import models
 from django.conf import settings
 from notifications.base.models import AbstractNotification
-
+from django.contrib.postgres.fields import ArrayField
 
 # class Notification(AbstractNotification):
     # custom fields will go here
@@ -23,3 +24,30 @@ class DeviceInformation(models.Model):
 
     def __str__(self):
         return self.name
+
+class SupportedAppVersion(models.Model):
+    UPGRAGE_TYPE = (
+        ('FORCEUPGRADE', 'force upgrade'),
+        ('RECOMMENDUPGRADE','recommend upgrade')
+    )
+    APP_TYPE = (
+        ('CLIENT', 'client'),
+        ('PRO','pro')
+    )
+    PLATFORM = (
+        ('ANDROID','android'),
+        ('APPLE','apple')
+    )
+
+    app_type = models.CharField(choices=APP_TYPE, default=None, max_length=100)
+    platform = models.CharField(choices=PLATFORM, default=None, max_length=100)
+    upgrade_type = models.CharField(choices=UPGRAGE_TYPE, default=None, max_length=100)
+    app_version = ArrayField(models.CharField(max_length=200), blank=True)
+
+    def __str__(self):
+        return self.app_type
+
+    class Meta:
+        verbose_name_plural = 'Supported App Versions'
+
+
