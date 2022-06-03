e05a55164a9b7c995329a1487ecc043367775f44
reiikennedy <mutuakennedy81@gmail.com>
2022-06-03 21:47:36 +0300

    feat: add referral app model serializers

---
+++ referral/apis/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+from django.contrib.auth import get_user_model
+from .. import models
+
+# referencing the custom user model
+User = get_user_model()
+
+class ReferralSystemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ReferralSystem
+        fields = ['pk', "tier_name", "reward_price", "second_reward_price", "description", "active"]
+
+
+class RecruiterSerializer(serializers.ModelSerializer):
+    referral_system = ReferralSystemSerializer(many=False, read_only=True)
+    payouts = serializers.SerializerMethodField()
+    class Meta:
+        model = models.Recruiter
+        fields = ['pk', "referral_system", "recruiter", "referral_code", "referrals",
+        "payouts"
+        ]
+
+    def get_payouts(self, object):
+        payouts = object.recruiter_payout.all()
+        return ReferralPayoutsSerializer(payouts,many=True).data
+    
+class ReferralPayoutsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ReferralPayouts
+        fields = ['pk', "recruiter", "amount", "is_paid"]
