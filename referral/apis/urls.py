b65126f288655252e707ba92e4ed88797f014a4a
reiikennedy <mutuakennedy81@gmail.com>
2022-06-27 06:05:00 +0300

    feat: add api endpoints for create-update apiViews

--- referral/apis/urls.py
+++ referral/apis/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('referral/payouts/list/', views.ReferralPayoutsListApi.as_view(), name='referral_payouts'),
     path('referral/system/list/', views.ReferralSystemListApi.as_view(), name='referral_system'),
     path('referral/code/submit/', views.validate_referral_code, name='submit_referral_code'),
+    path('referral/recruiter/create/', views.create_recruiter_profile, name='RecruiterCreateApi'),
+    path('referral/recruiter/<int:pk>/update/', views.RecruiterUpdateApi.as_view(), name='RecruiterUpdateApi'),
 ]
