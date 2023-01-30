d3ff1f4bdd6c0732c7f4f359490768d1077634a2
reiikennedy <mutuakennedy81@gmail.com>
2023-01-30 14:34:11 +0300

    add migrate sites to bash

--- docker-services/scripts/run.sh
+++ docker-services/scripts/run.sh
@@ -8,6 +8,7 @@ ls -la /vol/web
 
 whoami
 
+python manage.py migrate sites
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn homey.wsgi:application --bind 0.0.0.0:9000 --workers 2 
