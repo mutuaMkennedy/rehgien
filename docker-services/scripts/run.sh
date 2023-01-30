517741d147b23d52fe4e7df4dc5b8e1de547bacf
reiikennedy <mutuakennedy81@gmail.com>
2023-01-31 00:01:15 +0300

    remove migrate sites command

--- docker-services/scripts/run.sh
+++ docker-services/scripts/run.sh
@@ -8,7 +8,6 @@ ls -la /vol/web
 
 whoami
 
-python manage.py migrate sites
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn homey.wsgi:application --bind 0.0.0.0:9000 --workers 2 
