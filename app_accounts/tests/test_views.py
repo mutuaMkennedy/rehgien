6866ce7ea5daaff0f7c0829a48bf0a78f358aba1
reiikennedy <mutuakennedy81@gmail.com>
2023-03-19 00:53:44 +0300

    feat: add new tests method to user login

--- app_accounts/tests/test_views.py
+++ app_accounts/tests/test_views.py
@@ -20,8 +20,18 @@ class UserLoginViewTests(TestCase):
         self.client = Client()
 
         # Create two users
-        client_user = User.objects.create_user(username='test_client', password='1X<ISRUkw+tuK', user_type="CLIENT")
-        professional_user = User.objects.create_user(username='test_pro', password='2HJ1vRV0Z&3iD', user_type="PRO")
+        client_user = User.objects.create_user(
+            username='test_client', 
+            password='1X<ISRUkw+tuK', 
+            email="testclient@email.com",
+            phone="+254123456789",
+            user_type="CLIENT")
+        professional_user = User.objects.create_user(
+            username='test_pro', 
+            password='2HJ1vRV0Z&3iD', 
+            email="testpro@email.com",
+            phone="+254123456788",
+            user_type="PRO")
 
         client_user.save()
         professional_user.save()  
@@ -88,3 +98,123 @@ class UserLoginViewTests(TestCase):
         
         # Check our user gets redirected to the correct url
         self.assertRedirects(response, reverse('rehgien_pro:dashboard_home'))
+
+    def test_pro_user_can_log_in_via_email(self):
+        """ 
+            Test that user can log in using their email after submit of correct data
+        """
+        #  Issue POST request
+        response = self.client.post(reverse('app_accounts:user_login'), {"identity":"testpro@email.com","password":"2HJ1vRV0Z&3iD"})
+        
+        # Check our user gets redirected to the correct url meaning they have been authenticated
+        self.assertRedirects(response, reverse('rehgien_pro:dashboard_home'))
+
+    def test_pro_user_can_log_in_via_phone(self):
+        """ 
+            Test that user can log in using their phone number after submit of correct data
+        """
+        #  Issue POST request
+        response = self.client.post(reverse('app_accounts:user_login'), {"identity":"+254123456788","password":"2HJ1vRV0Z&3iD"})
+        
+        # Check our user gets redirected to the correct url meaning they have been authenticated
+        self.assertRedirects(response, reverse('rehgien_pro:dashboard_home'))
+
+    def test_client_user_cannot_login_using_email(self):
+        """ 
+            Test that client user cannot log in to protected view 
+            using their email after even on submit of correct data,
+            Only pro users should login from view
+        """
+        #  Issue POST request
+        response = self.client.post(reverse('app_accounts:user_login'), {"identity":"testclient@email.com","password":"1X<ISRUkw+tuK"})
+        
+        # Check our user is shown correct template
+        self.assertTemplateUsed(response, 'app_accounts/login.html')
+
+        # Check our user gets a permission denied message
+        self.assertIn("permission_denied", response.context)
+
+    def test_client_user_cannot_login_using_phone(self):
+        """ 
+            Test that client user cannot log in to protected view 
+            using their phone after even on submit of correct data,
+            Only pro users should login from view
+        """
+        #  Issue POST request
+        response = self.client.post(reverse('app_accounts:user_login'), {"identity":"+254123456789","password":"1X<ISRUkw+tuK"})
+        
+        # Check our user is shown correct template
+        self.assertTemplateUsed(response, 'app_accounts/login.html')
+
+        # Check our user gets a permission denied message
+        self.assertIn("permission_denied", response.context)
+
+    def test_user_is_shown_correct_template_when_they_enter_incorrect_login_data(self):
+        """ 
+            Test that client user cannot log if they enter wrong data and
+            they are shown the correct template with message
+        """
+        #  Issue POST request
+        response = self.client.post(reverse('app_accounts:user_login'), {"identity":"wrong_identity","password":"1X<ISRUkw+tuK"})
+        
+        # Check our user is shown correct template
+        self.assertTemplateUsed(response, 'app_accounts/login.html')
+
+        # Check our user gets a invalid identity message
+        self.assertIn("invalid_identity", response.context)
+
+    def test_user_is_shown_never_logged_in_if_email_provided_is_not_associated_with_a_user(self):
+        """ 
+            Test that client user cannot log if they enter email address
+            that is not connected to any account
+        """
+        #  Issue POST request
+        response = self.client.post(reverse('app_accounts:user_login'), {"identity":"invalidemail@email.com","password":"1X<ISRUkw+tuK"})
+        
+        # Check our user is shown correct template
+        self.assertTemplateUsed(response, 'app_accounts/login.html')
+
+        # Check our user gets an account not found message
+        self.assertIn("account_not_found", response.context)
+
+    def test_user_is_shown_never_logged_in_if_phone_provided_is_not_associated_with_a_user(self):
+        """ 
+            Test that client user cannot log if they enter a phone number
+            that is not connected to any account
+        """
+        #  Issue POST request
+        response = self.client.post(reverse('app_accounts:user_login'), {"identity":"+254123455789","password":"1X<ISRUkw+tuK"})
+        
+        # Check our user is shown correct template
+        self.assertTemplateUsed(response, 'app_accounts/login.html')
+
+        # Check our user gets an account not found message
+        self.assertIn("account_not_found", response.context)
+
+    def test_user_is_shown_correct_template_when_they_enter_correct_email_but_incorrect_password(self):
+        """ 
+            Test that client user cannot log if they enter a correct email address
+            but provide wrong password
+        """
+        #  Issue POST request
+        response = self.client.post(reverse('app_accounts:user_login'), {"identity":"testclient@email.com","password":"wrong_password"})
+        
+        # Check our user is shown correct template
+        self.assertTemplateUsed(response, 'app_accounts/login.html')
+
+        # Check our user gets incorrect password message
+        self.assertIn("incorrect_password", response.context)
+
+    def test_user_is_shown_correct_template_when_they_enter_correct_phone_but_incorrect_password(self):
+        """ 
+            Test that client user cannot log if they enter a correct email address
+            but provide wrong password
+        """
+        #  Issue POST request
+        response = self.client.post(reverse('app_accounts:user_login'), {"identity":"+254123456789","password":"wrong_password"})
+        
+        # Check our user is shown correct template
+        self.assertTemplateUsed(response, 'app_accounts/login.html')
+
+        # Check our user gets a incorrect password message
+        self.assertIn("incorrect_password", response.context)
